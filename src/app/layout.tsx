2f4302dd8909ff7e3ff5c574ac8f252e64f940a7
Oze Botach <oze@obotach.com>
2024-04-22 13:55:00 -0700

    📝 (layout.tsx): Fix formatting and syntax issues in the layout component The changes made in the layout component include fixing formatting and syntax issues such as adding missing semicolons, correcting import statements, and ensuring consistent code styling. These changes improve the readability and maintainability of the codebase.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,30 +1,31 @@
 import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
 import "./globals.css";
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/react";
 import ReactQueryProvider from "@/components/ReactQueryProvider";
-import { Toaster } from 'sonner';
-import { ClerkProvider } from '@clerk/nextjs'
+import { Toaster } from "sonner";
+import { ClerkProvider } from "@clerk/nextjs";
 import { CreatorBadge } from "@/components/creator-badge";
-import { LogSnagProvider } from '@logsnag/next';
+import { LogSnagProvider } from "@logsnag/next";
 import Navbar from "@/components/navbar";
 import SegmantAnalytics from "@/components/segment-analytics";
 import { HelpButton } from "@/components/plain-support/helpButton";
 import { CSPostHogProvider } from "@/lib/posthog";
-import { OpenpanelProvider } from '@openpanel/nextjs';
-import Script from 'next/script';
-import { HighlightInit } from '@highlight-run/next/client'
+import { OpenpanelProvider } from "@openpanel/nextjs";
+import Script from "next/script";
+import { HighlightInit } from "@highlight-run/next/client";
 
 const roboto = Roboto({
-  weight: ['400', '700'],
-  style: ['normal', 'italic'],
-  subsets: ['latin'],
-  display: 'swap',
-})
+  weight: ["400", "700"],
+  style: ["normal", "italic"],
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export const metadata: Metadata = {
   title: "JoblessDev - Where Jobless Developers find Jobs",
-  description: "JoblessDev is a job board for developers who are looking for work. Find your next job here!",
+  description:
+    "JoblessDev is a job board for developers who are looking for work. Find your next job here!",
 };
 
 export default function RootLayout({
@@ -32,19 +33,24 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-
   return (
     <ClerkProvider>
       <html lang="en">
         <head>
-          <LogSnagProvider token={process.env.LOGSNAG_API_KEY || ""} project={process.env.LOGSNAG_PROJECT_NAME || ""} />
+          <LogSnagProvider
+            token={process.env.LOGSNAG_API_KEY || ""}
+            project={process.env.LOGSNAG_PROJECT_NAME || ""}
+          />
           <Script
             async
             src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.NEXT_PUBLIC_GOOGLE_ADS_CLIENT_ID}`}
             strategy="lazyOnload"
             crossOrigin="anonymous"
           />
-          <meta name="google-adsense-account" content="ca-pub-4395499869686300" />
+          <meta
+            name="google-adsense-account"
+            content="ca-pub-4395499869686300"
+          />
         </head>
         <body className={roboto.className}>
           <ReactQueryProvider>
@@ -60,7 +66,7 @@ export default function RootLayout({
               <HighlightInit
                 projectId={process.env.NEXT_PUBLIC_HIGHLIGHT_PROJECT_ID || ""}
                 serviceName="my-nextjs-frontend"
-                tracingOrigins
+                tracingOrigins={[/^(?!.*\.clerk\..*).*/]}
                 networkRecording={{
                   enabled: true,
                   recordHeadersAndBody: true,
